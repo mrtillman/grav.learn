cf31a228da59926b0da2d3553f3cddf42b0bc44f
mrtillman <askmrtillman@gmail.com>
2019-02-09 16:10:58 -0500

    update readme + revise folder structure

--- runner.js
+++ runner.js
@@ -3,7 +3,7 @@
 var adventure = require('adventure');
 var shop = adventure('example-adventure');
 
-var problems = [ 'dinosaurs', 'robots', 'wowsers' ];
+var problems = [ 'grav.exists', 'grav.address', 'grav.test' ];
 problems.forEach(function (prob) {
     shop.add(prob, function () { return require('./problems/' + prob) });
 });
