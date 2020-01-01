8a9abf86769334c07bc41717442f81deac0c9a0b
mrtillman <askmrtillman@gmail.com>
2020-01-01 16:03:32 -0500

    remove dead code

--- solutions/image-check.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (client) {
-  let result = await client.exists();
-  let exists = result.Value.exists;
-  while(exists){
-    await client.removeImage();
-    result = await client.exists();
-    exists = result.Value.exists;
-  }
-}
