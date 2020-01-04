8b7e05081dc3a626130c15342d4c424291a29975
mrtillman <askmrtillman@gmail.com>
2020-01-04 15:03:55 -0500

    simplify test verification; fix imports + breaking changes

--- problems/check-primary-image/index.js
+++ problems/check-primary-image/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const verify = require('adventure-verify');
-const { mockClient, ProblemType } = require('../../mock-factory');
+const { mockClient } = require('../../mock-factory');
+const ProblemTypes =require('../problem-types');
 
 exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
@@ -9,13 +10,11 @@ exports.solution = fs.createReadStream(__dirname + '/solution.txt');
 exports.verify = verify({ modeReset: true }, async function (args, test) {
     const solutionFilePath = args[0];
     const solution = require(path.resolve(solutionFilePath));
-    test.equal(typeof solution, 'function', 'you exported a function');
-    const { client, spies } = await mockClient(ProblemType.CheckPrimaryImage);
+    const { client } = await mockClient(ProblemTypes.CheckPrimaryImage);
     await solution(client);
-    let calledExists = spies.existsMethod.calledBefore(spies.removeImageMethod);
-    let checkedExistsField = spies.existsProperty.get.calledBefore(spies.removeImageMethod);
-    test.equal((calledExists && checkedExistsField), true, "you checked the primary image");
-    test.equal(spies.removeImageMethod.calledAfter(spies.existsMethod), true, "you removed the primary image");
-    test.equal(spies.existsMethod.calledAfter(spies.removeImageMethod), true, "you checked the primary image again");
+    test.equal(typeof solution, 'function', 'you exported an async function');
+    test.equal(client.didCheckPrimaryImage(), true, "you checked if the primary image exists");
+    test.equal(client.didRemovePrimaryImage(), true, "you removed the primary image");
+    test.equal(client.didCheckPrimaryImageAgain(), true, "you checked the primary image again");
     test.end();
 });
