b325b04f80ac321fc8c9ae06d235efdc56dee3ce
mrtillman <askmrtillman@gmail.com>
2020-01-04 14:08:04 -0500

    move test setup logic to mock factory

--- problems/check-primary-image/index.js
+++ problems/check-primary-image/index.js
@@ -1,26 +1,7 @@
-const sinon = require('sinon');
 const fs = require('fs');
 const path = require('path');
 const verify = require('adventure-verify');
-const { GravatarClient } = require('grav.client');
-const { ExistsMethodResponse, RemoveImageMethodResponse } = require('grav.client/Release/Domain/method-responses');
-const { Result } = require('grav.client/Release/Common/result');
-const stub = require('grav.client/Release/Common/TestDoubles/json-response-stubs');
-
-function existsResult(useSuccess){
-    const response = new ExistsMethodResponse("");
-    response.json = stub.existsJsonResponse;
-    response.parseMembers();
-    response.exists = useSuccess;
-    return Promise.resolve(Result.Ok(response));
-}
-
-function removeImageResult(){
-    const response = new RemoveImageMethodResponse("");
-    response.json = stub.removeImageJsonResponse;
-    response.parseMembers();
-    return Promise.resolve(Result.Ok(response));
-}
+const { mockClient, ProblemType } = require('../../mock-factory');
 
 exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
@@ -29,21 +10,12 @@ exports.verify = verify({ modeReset: true }, async function (args, test) {
     const solutionFilePath = args[0];
     const solution = require(path.resolve(solutionFilePath));
     test.equal(typeof solution, 'function', 'you exported a function');
-    const client = new GravatarClient();
-    const existsMethod = sinon.stub();
-    const existsResponse = await existsResult(true);
-    const existsProperty = sinon.spy(existsResponse.Value, "exists", ["get"]);
-    const removeImageMethod = sinon.stub();
-    existsMethod.onFirstCall().returns(Promise.resolve(existsResponse));
-    existsMethod.onSecondCall().returns(existsResult(false));
-    removeImageMethod.onFirstCall().returns(removeImageResult());
-    client.exists = existsMethod;
-    client.removeImage = removeImageMethod;
+    const { client, spies } = await mockClient(ProblemType.CheckPrimaryImage);
     await solution(client);
-    let calledExists = existsMethod.calledBefore(removeImageMethod);
-    let checkedExistsField = existsProperty.get.calledBefore(removeImageMethod);
+    let calledExists = spies.existsMethod.calledBefore(spies.removeImageMethod);
+    let checkedExistsField = spies.existsProperty.get.calledBefore(spies.removeImageMethod);
     test.equal((calledExists && checkedExistsField), true, "you checked the primary image");
-    test.equal(removeImageMethod.calledAfter(existsMethod), true, "you removed the primary image");
-    test.equal(existsMethod.calledAfter(removeImageMethod), true, "you checked the primary image again");
+    test.equal(spies.removeImageMethod.calledAfter(spies.existsMethod), true, "you removed the primary image");
+    test.equal(spies.existsMethod.calledAfter(spies.removeImageMethod), true, "you checked the primary image again");
     test.end();
 });
