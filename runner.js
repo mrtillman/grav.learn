27f174a0866c9b3b0ffb95a6163c138d7f5bcbd9
Stephen Tillman <askmrtillman@gmail.com>
2020-04-19 22:24:43 -0400

    number each challenge

--- runner.js
+++ runner.js
@@ -4,15 +4,16 @@ const adventure = require('adventure');
 const shop = adventure('grav.learn');
 
 const problems = [ 
-    { title: 'CHECK PRIMARY IMAGE', name: 'check-primary-image' },
-    { title: 'GET EMAIL ADDRESSES', name: 'get-email-addresses' },
-    { title: 'FIND IMAGES', name: 'find-images' },
-    { title: 'FIND SINGLE IMAGE', name: 'find-single-image' },
-    { title: 'UPLOAD X-RATED IMAGE', name: 'x-rated-image' },
-    { title: 'SET NEW PRIMARY IMAGE', name: 'new-primary' },
-    { title: 'LOAD PREVIOUS IMAGE', name: 'load-previous-image' },
-    { title: 'LOAD NEXT IMAGE', name: 'load-next-image' },
-    { title: 'SET DEFAULT ICON', name: 'set-default-icon' }
+    { title: '0. CHECK PRIMARY IMAGE', name: 'check-primary-image' },
+    { title: '1. GET EMAIL ADDRESSES', name: 'get-email-addresses' },
+    { title: '2. FIND IMAGES', name: 'find-images' },
+    { title: '3. FIND SINGLE IMAGE', name: 'find-single-image' },
+    { title: '4. UPLOAD X-RATED IMAGE', name: 'x-rated-image' },
+    { title: '5. SET NEW PRIMARY IMAGE', name: 'new-primary' },
+    { title: '6. LOAD PREVIOUS IMAGE', name: 'load-previous-image' },
+    { title: '7. LOAD NEXT IMAGE', name: 'load-next-image' },
+    { title: '8. SET DEFAULT ICON', name: 'set-default-icon' },
+    { title: '9. CREATE A USE CASE', name: 'create-use-case' }
 ];
 
 problems.forEach(function (prob) {
