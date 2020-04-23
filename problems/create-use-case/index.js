ee548baec88435f6c5fd8798ee8b5072a65593f0
Stephen Tillman <askmrtillman@gmail.com>
2020-04-22 22:01:56 -0400

    refactor verification logic

--- problems/create-use-case/index.js
+++ problems/create-use-case/index.js
@@ -7,21 +7,38 @@ const ProblemTypes =require('../problem-types');
 exports.problem = fs.createReadStream(__dirname + '/problem.txt');
 exports.solution = fs.createReadStream(__dirname + '/solution.txt');
 
+function isConstructor(f) {
+    try {
+      new f();
+    } catch (err) {
+      // verify err is the expected error and then
+      return false;
+    }
+    return true;
+  }
+
 exports.verify = verify({ modeReset: true }, async function (args, test) {
     const solutionFilePath = args[0];
     const solution = require(path.resolve(solutionFilePath));
-    const useCase = new solution();
-    
-    const didImplementUseCase = (
-        typeof useCase == 'object' && !!useCase.execute
-    );
-    
-    test.equal(didImplementUseCase, true, 'you implemented a use case');
+    const didReturnClass = isConstructor(solution);
+    let useCase;
 
-    const client = await mockClient(ProblemTypes.CreateUseCase);
-    useCase.client = client;
-    const result = await useCase.execute();
-    
-    test.equal(client.didFindUserAddress(result), true, "your use case found the user address");
+    const didImplementUseCase = function(){
+        if(!didReturnClass) { return false }
+        useCase = new solution();
+        return !!useCase.execute;
+    }
+
+    const didFindUserAddress = async function(){
+        if(!didReturnClass || !useCase.execute) { return false }
+        const client = await mockClient(ProblemTypes.CreateUseCase);
+        useCase.client = client;
+        const result = await useCase.execute();
+        return client.didFindUserAddress(result)
+    }
+
+    test.equal(didReturnClass, true, 'you returned a class');
+    test.equal(didImplementUseCase(), true, 'you implemented a use case');
+    test.equal(await didFindUserAddress(), true, "your use case found the user address");
     test.end();
 });
